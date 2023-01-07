7919fb7abc596e519ec0242ac6f73966a28720ac
Judith Baidoo <judithbaidoo@Judiths-MacBook-Pro-2.local>
2023-01-07 02:57:38 +0000

    updates

---
+++ starter/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const header = () => {
+    return(
+        <div className="list-books-title">
+            <h1>MyReads</h1>
+        </div>
+
+    )
+}
+
+export default header ;
---
+++ starter/src/components/Shelf.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Book from './Book';
+
+const Shelf = ({books}) => {
+    return(
+        <div className="bookshelf">
+                <h2 className="bookshelf-title">Currently Reading</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {books.map( b => (
+                      <li>
+                        <Book book = {b} />
+                      </li>
+
+                    ))}
+                    
+                  </ol>
+                </div>
+              </div>
+    )
+
+}
+
+export default Shelf;
